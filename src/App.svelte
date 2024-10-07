0e167ac7b67a775db580a01a6bb01ea0b97737d3
Juliana <juliana.g.bello@gmail.com>
2024-10-07 10:09:05 -0300

    svelte

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+	let name = 'world';
+	let src = '/public/build/favicon.png';
+</script>
+
+
+<img src={src} alt="favicon" />
+
+<h1> Hello {name.toUpperCase()} </h1> 
+<p>paragrafo</p>
+
+
+<style>
+	p {
+		color: blue;
+		font-family: 'Comic Sans MS', cursive;
+		font-size: 2em;
+	}
+	h1{
+		color: red;
+		font-family: 'Comic Sans MS', cursive;
+		font-size: 2em;
+	}
+</style>
